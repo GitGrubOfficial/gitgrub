3cf8929a2e4964f1d6faccf2bb78bfda66e7f5cf
Thomas Nguyen <tnguyen306@gatech.edu>
2025-04-16 01:34:27 -0700

    getting user model to work:

--- backend/recipes/views/recipe_views/recipe_list.py
+++ backend/recipes/views/recipe_views/recipe_list.py
@@ -2,15 +2,11 @@ from rest_framework.generics import ListAPIView
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
+
 
 class RecipeListView(ListAPIView):
     serializer_class = RecipeSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        owner_id = self.request.query_params.get("owner")
-        if owner_id:
-            return Recipe.objects.filter(owner_id=owner_id)
-        return Recipe.objects.all()
+        return Recipe.objects.filter(owner=self.request.user)
